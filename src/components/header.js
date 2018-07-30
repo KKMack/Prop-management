ecb5b00b22cd4a6e52d459ed6f657cb108d9b0de
Kaden <you@example.com>
2018-07-30 09:39:05 -0600

    refactored grid

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <div className="header">
+            <h1 className="header__title">Welcome to Kaden's Connections!</h1>
+            <p className="header__subtitle">Please login to continue</p>
+        </div>
+    )
+}
+
+export function HeaderBar() {
+    return (
+        <div className="bar"></div>
+    )
+}
