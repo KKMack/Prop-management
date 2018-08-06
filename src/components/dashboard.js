e0a10bd88964cfa87054b81eb229033f65b9b1e4
Kaden <you@example.com>
2018-08-06 11:41:50 -0600

    built tabnav structure

---
+++ src/components/dashboard.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Tabnav from './tabnav'
+
+class Dashboard extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            tabs: [
+                {
+                    title: 'Newsletter',
+                    active: false,
+                    component: <h4>Hey There - Newsletter</h4>
+                },
+                {
+                    title: 'Requests',
+                    active: false,
+                    component: <h4>Hey There - Requests</h4>
+                },
+            ]
+        }
+    }
+    render() {
+      return <Tabnav tabs={this.state.tabs}/>;
+      }
+  }
+
+  export default Dashboard;
