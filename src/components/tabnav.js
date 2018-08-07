8917df9464946448becd52e18e07d9627d9ee8d1
Kaden <you@example.com>
2018-08-07 08:52:36 -0600

    tabnav grid

--- src/components/tabnav.js
+++ src/components/tabnav.js
@@ -4,10 +4,17 @@ class Tabnav extends Component {
     render() {
         return (
         <div className="tab-nav">
-        
+            <div className="tab-nav__tabs">
+            {
+                this.props.tabs.map((tab, index) => {
+                    return <a className="tab-nav__tab">{tab.title}</a>
+                })
+            }
+            </div>
+            <div>Request or newsletter goes here</div>
         </div>
         )
     }
 }
 
-  export default Tabnav;
+export default Tabnav;
