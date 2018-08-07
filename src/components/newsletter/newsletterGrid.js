08b5e6e0800f7aa18c2a9a260bba01e7d03de900
Kaden <you@example.com>
2018-08-07 10:18:16 -0600

    built newsletter and date functionality

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import NewsletterBox from './newsletterBox'
 
 class NewsletterGrid extends Comment {
     render() {
         return (
             <div className="newsletter-grid">
-                fdjskla;
+            {/* add button */}
+            <NewsletterGrid date={new Date()}/>
+            {/* archive */}
+            {/* content */}
             </div>
         )
     }
