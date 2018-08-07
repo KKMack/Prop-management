3ef2c740254450f3e79d3aa28bb24fd7814cf06a
Kaden <you@example.com>
2018-08-07 09:57:47 -0600

    built the newsletter grid component and grid

---
+++ src/components/newsletter/newsletterGrid.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NewsletterGrid extends Comment {
+    render() {
+        return (
+            <div className="newsletter-grid">
+                fdjskla;
+            </div>
+        )
+    }
+}
+
+export default NewsletterGrid;
