d4e8b58ee68456c38b9d9cef101c8735ce9201df
Kaden <you@example.com>
2018-08-08 09:21:27 -0600

    mapped over a newsletter JSON item to the latest newsletter component

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -5,12 +5,21 @@ import NewsletterLatest from './newsletterLatest'
 
 class NewsletterGrid extends Comment {
     render() {
+
+        const latest = {
+            _id: '11',
+            title: 'first post',
+            body: 'You should be very greatful that I am taking the time to write this. I could have just gone and put in some random Lorem Ipsum text and called it good, but I decided that I would rather prove myself to you by writing out this massive section of text. You\'re welcome.',
+            date: new Date(),
+            imgUrl: 'http://via.placeholder.com/960x258'
+        }
+
         return (
             <div className="newsletter-grid">
             {/* add button */}
             <NewsletterBox date={new Date()}/>
             <NewsletterArchive/>
-            {/* content */}
+            <NewsletterLatest {...latest}/>
             </div>
         )
     }
--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 class NewsletterLatest extends Component {
     render() {
+        const { title, imageUrl, body } = this.props;
         return (
             <div className='newsletter-latest'>
-                <h1 className="newsletter-latest__title">Title Goes Here</h1>
-                <img className='newsletter-latest__image' src="http://via.placeholder.com/960x258"/>
+                <h1 className="newsletter-latest__title">{title}</h1>
+                <img className='newsletter-latest__image' src={imageUrl}/>
                 <div className="newsletter-latest__body">
-                    <p>You should be very greatful that I am taking the time to write this. I could have just gone and put in some random Lorem Ipsum text and called it good, but I decided that I would rather prove myself to you by writing out this massive section of text. You're welcome.</p>
+                    <p>{body}</p>
                 </div>
             </div>
         )
